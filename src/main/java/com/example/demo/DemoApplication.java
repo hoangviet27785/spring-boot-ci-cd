4878e6a933751f57f50fe20c4c3b8e25a1aab58b
Hoang Viet <hoangviet27785@gmail.com>
2026-05-25 12:35:59 +0700

    update source

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot CI/CD!";
+        return "Hello from Spring Boot CI/CD new update!";
     }
 }
 
